25fbe3ff08ee8162710349fc56970ac5eef74276
LOOR-ZAMBRANO-ROBERTH-DAVID <56706913+LOOR-ZAMBRANO-ROBERTH-DAVID@users.noreply.github.com>
2019-10-17 21:11:13 -0500

    Add files via upload

---
+++ Ejercicio1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoberthLoor3B
+{
+    class Ejercicio1
+    {
+        public static void nummayor() {
+            Console.Write("\tIngrese el numero de datos a ingresar: ");
+            int datos = Convert.ToInt32(Console.ReadLine());
+
+            int i = 0, mayor = 0;
+            while (i < datos) {
+                i++;
+                Console.Write("\t\tIngrese el termino entero: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                if (num > mayor) { mayor = num;  }
+            }
+            Console.Write("\t\tEl numero mayor es: " +mayor);
+            Console.ReadKey();
+            Program.menu();
+
+        }
+
+    }
+}
